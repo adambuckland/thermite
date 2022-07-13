a6cc31cfae493e7579be0e6511604f1befc91103
Adam Buckland <adambuckland@users.noreply.github.com>
2022-07-13 13:42:16 +0100

    Add base winit implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+mod core;
+
+use std::error::Error;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, ElementState, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{WindowBuilder};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Thermite")
+        .with_inner_size(LogicalSize::new(800,600))
+        .build(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => { *control_flow = ControlFlow::Exit },
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput {
+                    input: KeyboardInput {
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        state: ElementState::Released, ..
+                    }, ..
+                }, ..
+            } => { *control_flow = ControlFlow::Exit },
+            _ => {}
+        }
+    });
 }
